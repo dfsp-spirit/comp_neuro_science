d43d70f9ec811701f3746b53a345e15c7e643874
Tim Schäfer <ts+code@rcmd.org>
2018-01-01 17:12:04 +0100

    Make the plot show.

--- analog_data_processing/sine_waves.py
+++ analog_data_processing/sine_waves.py
@@ -1,4 +1,6 @@
 # Plots some sine wave signals, as well as their joint signal
+import matplotlib
+matplotlib.use('TkAgg') 
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -22,4 +24,4 @@ joint_signal = signals[0] + signals[1]
 ax = plt.subplot(3, 1, len(signal_frequencies_hz)+1)
 ax.plot(time_base, joint_signal,  c='k', lw=2)
 
-fig.show()
+plt.show()
