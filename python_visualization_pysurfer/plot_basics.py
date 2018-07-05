b02afa52fef654d6a736cb54fe5565d84a1ba851
Tim Schaefer <ts+code@rcmd.org>
2018-07-05 19:37:20 +0200

    Add most basic pysurfer example script.

---
+++ python_visualization_pysurfer/plot_basics.py
@@ -0,0 +1,25 @@
+"""
+Basic Visualization
+===================
+
+Initialize a basic visualization session.
+
+"""
+from surfer import Brain
+
+print(__doc__)
+
+"""
+Define the three important variables.
+Note that these are the first three positional arguments
+in tksurfer (and pysurfer for that matter).
+"""
+subject_id = 'fsaverage'
+hemi = 'lh'
+surf = 'inflated'
+
+"""
+Call the Brain object constructor with these
+parameters to initialize the visualization session.
+"""
+brain = Brain(subject_id, hemi, surf)
