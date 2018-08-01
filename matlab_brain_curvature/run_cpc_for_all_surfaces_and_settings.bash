340d86f561772cf9f914b48bf77b7101e678044b
Tim Schäfer <ts+code@rcmd.org>
2018-08-01 14:26:47 +0200

    fix typo that lead to bad substituion error

--- matlab_brain_curvature/run_cpc_for_all_surfaces_and_settings.bash
+++ matlab_brain_curvature/run_cpc_for_all_surfaces_and_settings.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-CPC_SCRIPT="~/develop/comp_neuro_science/matlab_brain_curvature/compute_principal_curvature.bash"
+CPC_SCRIPT="$HOME/develop/comp_neuro_science/matlab_brain_curvature/compute_principal_curvature.bash"
 
 SUBJECS_FILE="subjects_analysis.txt"
 APPTAG="[RCPC]"
@@ -13,6 +13,6 @@ for SURFACE in $SURFACES; do
         LABEL=".a${AVG}"
         LOGFILE="run_cpc_${SURFACE}_${AVG}.log"
         echo "$APPTAG Running cpc script for surface $SURFACE with averaging set to $AVG..."
-        $CPC_SCRIPT $SUBJECS_FILE $SURFACE $LABEL "-a ${$AVG}" | tee $LOGFILE
+        $CPC_SCRIPT $SUBJECS_FILE $SURFACE $LABEL "-a ${AVG}" | tee $LOGFILE
     done
 done
