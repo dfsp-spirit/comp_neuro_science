7eae35f5cc86371994afecaf65b8e7d5388acb4d
Tim Schäfer <ts+code@rcmd.org>
2018-08-01 11:11:58 +0200

    Add checks for mris_curvature binary, some generalizations

--- matlab_brain_curvature/compute_principal_curvature.bash
+++ matlab_brain_curvature/compute_principal_curvature.bash
@@ -3,6 +3,17 @@
 ## This script writes the curvature data into files in the $SUBJECTS_DIR/<subject_id>/surf/ directory, so you need write access to the $SUBJECTS_DIR to run it.
 ## Written by Tim Schäfer, 2018-07-30
 
+## Settings ##
+
+# path to the mris_curvature binary. It is fine like this if mris_curvature is on your PATH.
+MRIS_CURVATURE_BINARY=$(which mris_curvature)
+
+# The command line options passed to mris_curvature. (The input surface file gets added automatically, so do not add it here.)
+MRIS_CURVATURE_OPTIONS="-min -max -a 10"
+
+
+## Start of script ##
+
 APPTAG='[CPC]'
 echo "$APPTAG Compute Principal Curvature -- run the mris_curvature tool to compute k1 and k2 for all subjects in a subjects file."
 
@@ -14,8 +25,21 @@ if [ -z "$1" -o -z "$2" ]; then
     exit 1
 fi
 
+if [ -z "$MRIS_CURVATURE_BINARY" ]; then
+    echo "$APPTAG ERROR: Could not find mris_curvature binary on the PATH. Please add its location to your PATH or set the path in this script."
+    exit 1
+fi
+
+if [ ! -x "$MRIS_CURVATURE_BINARY" ]; then
+    echo "$APPTAG ERROR: Could not find executable mris_curvature binary in path '$MRIS_CURVATURE_BINARY'. Please check the path and/or make sure it is executable."
+    exit 1
+fi
+
 SUBJECTSFILE="$1"
 SURFACE="$2"
+
+echo "$APPTAG Using subjects from file '$SUBJECTSFILE' and computing curvatures for '$SURFACE' surface."
+echo "$APPTAG Running mris_curvature from '$MRIS_CURVATURE_BINARY' with options '$MRIS_CURVATURE_OPTIONS'."
 
 BASEDIR=$(pwd)
 
@@ -35,7 +59,7 @@ for SUBJECT in $ALL_SUBJECT_IDS; do
     NUM_SUBJECTS=$((NUM_SUBJECTS + 1))
     SUBJECT_SURF_DIR="${SUBJECT}/surf"
     if [ -d "$SUBJECT_SURF_DIR" ]; then
-        cd "$SUBJECT_SURF_DIR" && mris_curvature -min -max -a 3 rh.${SURFACE} && mris_curvature -min -max -a 3 lh.${SURFACE}
+        cd "$SUBJECT_SURF_DIR" && $MRIS_CURVATURE_BINARY $MRIS_CURVATURE_OPTIONS rh.${SURFACE} && $MRIS_CURVATURE_BINARY $MRIS_CURVATURE_OPTIONS lh.${SURFACE}
 
         retVal=$?
         if [ $retVal -ne 0 ]; then
