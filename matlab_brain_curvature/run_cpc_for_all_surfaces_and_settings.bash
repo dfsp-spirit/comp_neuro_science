9efadc05d9fdbb069a3d7250f4cfd481904cc6e0
Tim Schäfer <ts+code@rcmd.org>
2018-08-01 14:20:21 +0200

    Add wrapper script

---
+++ matlab_brain_curvature/run_cpc_for_all_surfaces_and_settings.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+CPC_SCRIPT="~/develop/comp_neuro_science/matlab_brain_curvature/compute_principal_curvature.bash"
+
+SUBJECS_FILE="subjects_analysis.txt"
+APPTAG="[RCPC]"
+
+# run the script for all settings
+SURFACES="pial white"
+AVERAGINGS="0 5 10 15 20"
+
+for SURFACE in $SURFACES; do
+    for AVG in $AVERAGINGS; do
+        LABEL=".a${AVG}"
+        LOGFILE="run_cpc_${SURFACE}_${AVG}.log"
+        echo "$APPTAG Running cpc script for surface $SURFACE with averaging set to $AVG..."
+        $CPC_SCRIPT $SUBJECS_FILE $SURFACE $LABEL "-a ${$AVG}" | tee $LOGFILE
+    done
+done
