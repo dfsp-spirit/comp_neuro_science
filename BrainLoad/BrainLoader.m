629162a21773116baee3ea067b00db2fcc01040a
Tim Schaefer <ts+code@rcmd.org>
2018-10-12 16:36:54 +0200

    parse conmmand line args. this is annoying in matlab.

--- BrainLoad/BrainLoader.m
+++ BrainLoad/BrainLoader.m
@@ -17,6 +17,9 @@ classdef BrainLoader
             % Specify available command line parameters and values
             
             check_hemi = @(x) any(validatestring(x, {'lh','rh', 'both'}));
+            default_subjects_dir = BrainLoader.get_env_or_default('SUBJECTS_DIR', '.');
+            
+            default_arg_empty_string = '';
                        
             addRequired(p, 'measure', @ischar);
             addParameter(p, 'hemi', 'both', check_hemi);
@@ -24,12 +27,15 @@ classdef BrainLoader
             addParameter(p, 'fwhm', '10', @ischar);
             addParameter(p, 'average_subject', 'fsaverage', @ischar);
             addParameter(p, 'subjects_file', 'subjects.txt', @ischar);
+            addParameter(p, 'subjects_dir', default_subjects_dir, @ischar);
+            addParameter(p, 'subjects_file_dir', default_arg_empty_string, @ischar);
             
           
             
             % Parameters specified, now parse them:
             parse(p, measure, varargin{:})
             
+            % Report
             if BrainLoader.debug == 1
                 disp(['measure: ',p.Results.measure])
                             
@@ -44,9 +50,34 @@ classdef BrainLoader
                 end
             end
             
+            % Based on the parameters we received, setup data.
+            subjects_dir = p.Results.subjects_dir;
+            
+            if strcmp(p.Results.subjects_file_dir, default_arg_empty_string)
+                subjects_file_dir = subjects_dir;     % If the directory was not supplied by the user, assume the file is in the subject_dir.
+            else
+                subjects_file_dir = p.Results.subjects_file_dir;
+            end
+            
+            % Report again.
+            if BrainLoader.debug == 1
+                disp(['subjects_dir: ', subjects_dir])
+                disp(['subjects_file_dir: ', subjects_file_dir]);
+            end
+            
+            
             group_morphology_data = strcat(measure, 'a');
             group_meta_data = 'a';
             
         end
+        
+        % Retrieves an env var, or the default value if it is not set.
+        function value = get_env_or_default(env_var, default_value)
+            value = getenv(env_var);
+            if strlength(value) == 0
+                value = default_value;
+            end
+        end
+        
     end
 end
