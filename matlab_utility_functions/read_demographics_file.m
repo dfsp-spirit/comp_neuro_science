1eb8321ebb98b738297a328f1aa60938fb96a9ef
Tim Schaefer <ts+code@rcmd.org>
2018-10-28 20:26:33 +0100

    return on length mismatch in parse string and var names

--- matlab_utility_functions/read_demographics_file.m
+++ matlab_utility_functions/read_demographics_file.m
@@ -1,10 +1,15 @@
 function res_struct = read_demographics_file(demographics_file, scan_string, variable_names)
+% Example usage:
+%  demographics = read_demographics_file(demographics_file, '%s %s %f %f %f %f %f %f %s', ["subjects", "group", "age", "iq", "bd1", "bd2", "bd3", "bd4", "site"]);
+%  subjects = demographics.subjects;
+
 fprintf("Reading demographics data from file %s ...\n", demographics_file);
 fh_demographics = fopen(demographics_file);
 
 format_entries = split(scan_string, " ");
 if length(format_entries) ~= length(variable_names)
-    fprintf("ERROR: Found %d format entries in scan_string and %d variable names. Lengths must be equal!", length(format_entries), length(variable_names));
+    fprintf("ERROR: Found %d format entries in scan_string and %d variable names. Lengths must be equal!\n", length(format_entries), length(variable_names));
+    return;
 end
 
 C = textscan(fh_demographics, scan_string);
