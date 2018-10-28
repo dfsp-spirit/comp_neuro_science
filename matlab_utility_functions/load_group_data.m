4d3af5f8c7294e3d8bafd3c032145627f34a49b1
Tim Schaefer <ts+code@rcmd.org>
2018-10-28 14:41:34 +0100

    rename old function to _cell, create new daa loading function that returns normal matrix instead of cell array.

--- matlab_utility_functions/load_group_data.m
+++ matlab_utility_functions/load_group_data.m
@@ -1,18 +1,19 @@
 function group_data = load_group_data(subjects_list, subjects_dir, measure, surf, fwhm)
 % Read group morphometry data. Requires surfstat in your MATLAB path, see http://www.math.mcgill.ca/keith/surfstat/.
 % Assumes that the morphometry data files are in mgh format and have been mapped to fsaverage (like lh.area.fwhm10.fsaverage.mgh).
-% Data is returned in a cell array with two cells. The first contains the subject ids as string, the second contains
-%  an array with the morphometry data for the respective subject.
+% Data is returned in a matrix containing the data of each subject on one row. The order of the input subjects_list is preserved in the returned group_data.
+
+%% Constants
+NUM_VERTS_PER_HEMI = 163842;       % for fsaverage
+NUM_VERTS_PER_BRAIN = NUM_VERTS_PER_HEMI * 2;
 
 num_subjects = length(subjects_list);
-C = cell(2, num_subjects);
-group_data = C;
+group_data = zeros(num_subjects, NUM_VERTS_PER_BRAIN);
 
 for subject_idx = 1:num_subjects
   subject_id = char(subjects_list(subject_idx));
-  subject_data = read_subject_avg_data(subject_id, subjects_dir, measure, surf, fwhm);
-  group_data{1, subject_idx} = subject_id;
-  group_data{2, subject_idx} = subject_data;
+  subject_data = read_subject_avg_data(subject_id, subjects_dir, measure, surf, fwhm)';
+  group_data(subject_idx, :) = subject_data;
 end
 
 end
---
+++ matlab_utility_functions/load_group_data_cell.m
@@ -0,0 +1,18 @@
+function group_data = load_group_data_cell(subjects_list, subjects_dir, measure, surf, fwhm)
+% Read group morphometry data. Requires surfstat in your MATLAB path, see http://www.math.mcgill.ca/keith/surfstat/.
+% Assumes that the morphometry data files are in mgh format and have been mapped to fsaverage (like lh.area.fwhm10.fsaverage.mgh).
+% Data is returned in a cell array with two cells. The first contains the subject ids as string, the second contains
+%  an array with the morphometry data for the respective subject.
+
+num_subjects = length(subjects_list);
+C = cell(2, num_subjects);
+group_data = C;
+
+for subject_idx = 1:num_subjects
+  subject_id = char(subjects_list(subject_idx));
+  subject_data = read_subject_avg_data(subject_id, subjects_dir, measure, surf, fwhm);
+  group_data{1, subject_idx} = subject_id;
+  group_data{2, subject_idx} = subject_data;
+end
+
+end
