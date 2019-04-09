3459f21213b441685873b0a9fadc0c68e0ba6c86
Tim Schaefer <ts+code@rcmd.org>
2019-04-09 18:13:56 +0200

    minor: add comment

--- matlab_utility_functions/write_csv_with_header.m
+++ matlab_utility_functions/write_csv_with_header.m
@@ -1,6 +1,6 @@
 function res = write_csv_with_header(csv_header_line, data_matrix, output_csv_file)
 % This function is very primitive, it only supports CSV in the sense that
-% the separator is really a comma ','.
+% the separator is really a comma ','. It writes data using dlmwrite, with its ceveats.
 %
 % So the header string should also be comma separated, of course.
 %
@@ -10,7 +10,7 @@ res = 0;
 if (csv_header_line == "")
     dlmwrite(output_csv_file, data_matrix);
 else
-    fid = fopen(output_csv_file, 'w'); 
+    fid = fopen(output_csv_file, 'w');
     fprintf(fid, '%s\n', csv_header_line);
     fclose(fid);
     dlmwrite(output_csv_file, data_matrix, '-append');
