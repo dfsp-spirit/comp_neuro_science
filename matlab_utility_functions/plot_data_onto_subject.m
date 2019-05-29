0a44e705af1f448eef68f2ec0142cebaf5693a61
Tim Schaefer <ts+code@rcmd.org>
2019-05-29 16:07:17 +0200

    modify function: do not show a surface if its data is empty

--- matlab_utility_functions/plot_data_onto_subject.m
+++ matlab_utility_functions/plot_data_onto_subject.m
@@ -9,32 +9,38 @@ subject_surf_dir = fullfile(subjects_dir, subject_id, 'surf');
 lh_display_surf_file = fullfile(subject_surf_dir, strcat('lh.', surf));
 rh_display_surf_file = fullfile(subject_surf_dir, strcat('rh.', surf));
 if exist(lh_display_surf_file, 'file') ~= 2
-    fprintf("ERROR: lh surf file '%s' does not exist. Check the path.\n", lh_display_surf_file);
+    error("lh surf file '%s' does not exist. Check the path.\n", lh_display_surf_file);
 end
 if exist(rh_display_surf_file, 'file') ~= 2
-    fprintf("ERROR: rh surf file '%s' does not exist. Check the path.\n", rh_display_surf_file);
+    error("rh surf file '%s' does not exist. Check the path.\n", rh_display_surf_file);
 end
 
-display_surface = SurfStatReadSurf ( {lh_display_surf_file, rh_display_surf_file} );
+if isempty(data_rh)
+    display_surface = SurfStatReadSurf ( {lh_display_surf_file} );
+elseif isempty(data_lh)
+    display_surface = SurfStatReadSurf ( {rh_display_surf_file} );
+else
+    display_surface = SurfStatReadSurf ( {lh_display_surf_file, rh_display_surf_file} );
+end
 
 measure_data = vertcat(data_lh, data_rh);
 
-figure;
+fig_handle = figure;
 
 
 SurfStatView(measure_data, display_surface, plot_title);
 SurfStatColormap(colmap);
 
 % Hack: delete the misplaced color bar (SurfStat bug)
-a=get(gcf,'Children');
-for i=1:length(a)
-    tag=get(a(i),'Tag');
-    if strcmp(tag,'Colorbar')
-        title=get(get(a(i),'Title'),'String');
-        delete(a(i));
-    end
-end
+% a=get(gcf,'Children');
+% for i=1:length(a)
+%     tag=get(a(i),'Tag');
+%     if strcmp(tag,'Colorbar')
+%         title=get(get(a(i),'Title'),'String');
+%         delete(a(i));
+%     end
+% end
 
-fig_handle = gcf;
+%fig_handle = gcf;
 
 end
