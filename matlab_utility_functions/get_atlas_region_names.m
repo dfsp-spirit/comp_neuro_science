c3c44850b67dec90b570e02607ff757d80ca009c
Tim Schaefer <ts+code@rcmd.org>
2019-07-05 13:20:18 +0200

    improve function, remove debug output

--- matlab_utility_functions/get_atlas_region_names.m
+++ matlab_utility_functions/get_atlas_region_names.m
@@ -1,15 +1,15 @@
-function region_names = get_atlas_region_names(subjects_dir_fsaverage, atlas_file_name)
+function region_names = get_atlas_region_names(subjects_dir, subject, atlas_name)
 % Loads the region names from an atlas file. Returns them as a string
 % array with shape (n, 1) for n structures in the file.
 %
-% subjects_dir_fsaverage: string, path to a directory containing the freesurfer 'fsaverage'
+% subjects_dir: string, path to a directory containing the freesurfer
 % subject.
-% atlas_file_name: string, name of the annotation file in the 'label' dir
-% of the 'fsaverage' subject. Usually something like 'lh.aparc.annot'. The
-% hemi part should not make a difference ('lh' or 'rh' lead to same results).
+% subject: string, subject name.
+% atlas_name: string, atlas name. Example: 'aparc' or 'aparc.a2009s'.
 %
 
-    aparc_file_this_hemi = fullfile(subjects_dir_fsaverage, 'fsaverage', 'label', atlas_file_name);
+    aparc_file_this_hemi = fullfile(char(subjects_dir), char(subject), 'label', char(sprintf("lh.%s.annot", atlas_name)));
+    %fprintf("Reading file '%s'.\n", aparc_file_this_hemi);
     [~, ~, colortable] = read_annotation(aparc_file_this_hemi);
-    region_names = string(colortable.struct_names);
+    region_names = string(matlab.lang.makeValidName(colortable.struct_names));
 end
