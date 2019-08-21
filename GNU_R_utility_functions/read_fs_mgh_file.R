5ea0e69d89732df1d548763687040d7e0c294eef
Tim Schaefer <ts+code@rcmd.org>
2019-08-21 09:24:51 +0200

    work on MGH parsing

--- GNU_R_utility_functions/read_fs_mgh_file.R
+++ GNU_R_utility_functions/read_fs_mgh_file.R
@@ -7,23 +7,58 @@ read_fs_mgh_file <- function(filepath) {
 
     # we could use 'gzfile' here if the file extension suggests it.
 
-    NEW_VERSION_MAGIC_NUMBER = 16777215;
-    fh = file(filepath, "rb");
-    magic_byte = fread3(fh);
     cat(sprintf("Parsing header of file '%s'.\n", filepath));
-    cat(sprintf(" *Magic byte is: %d\n", magic_byte));
-    num_verts = readBin(fh, integer(), n = 1, endian = "big");
-    num_faces = readBin(fh, integer(), n = 1, endian = "big");
-    values_per_vertex = readBin(fh, integer(), n = 1, endian = "big");
-    cat(sprintf(" *num_verts is %d, num_faces is %d, values_per_vertex is %d.\n", num_verts, num_faces, values_per_vertex));
+    fh = file(filepath, "rb");
+
+    v = fread1(fh);
+    ndim1 = fread1(fh);
+    ndim2 = fread1(fh);
+    ndim3  = fread1(fh);
+    nframes = fread1(fh);
+    dtype = fread1(fh);
+    dof = fread1(fh);
+
+
+    cat(sprintf(" v=%d, ndim1=%d, ndim2=%d, ndim3=%d, nframes=%d, type=%d, dof=%d.\n", v, ndim1, ndim2, ndim3, nframes, dtype, dof));
+
+
+    unused_header_space_size_left = 256;
+
+    ras_good_flag = freadshort(fh);
+    if(ras_good_flag == 1) {
+        cat(sprintf(" 'RAS good' flag is set, reading RAS information.\n"));
+        delta  = readBin(filehandle, float(), n = 3, endian = "big")
+        Mdc    = readBin(filehandle, float(), n = 9, endian = "big")
+        Pxyz_c = readBin(filehandle, float(), n = 3, endian = "big")
+        RAS_space_size = (3*4 + 4*3*4);
+        cat(sprintf(" Read %d bytes of RAS information.\n", RAS_space_size));
+        unused_header_space_size_left = unused_header_space_size_left - RAS_space_size;
+    } else {
+        cat(sprintf(" 'RAS good' flag is NOT set, no RAS information to read.\n"));
+    }
+    cat(sprintf(" There are %d header bytes left.\n", unused_header_space_size_left));
+
     cat(sprintf("Reading data.\n"));
-    data = readBin(fh, numeric(), size = 4, n = num_verts, endian = "big");
-    cat(sprintf("Data read, min=%f, max=%f.\n", min(data), max(data)));
+    nv = ndim1 * ndim2 * ndim3 * nframes;
+    volsz = c(ndim1, ndim2, ndim3, nframes);
+    cat(sprintf(" Expecting %d voxels total.\n", nv));
+
+    #data = readBin(fh, numeric(), size = 4, n = num_verts, endian = "big");
+    #cat(sprintf("Data read, min=%f, max=%f.\n", min(data), max(data)));
     close(fh);
 }
 
+# read a single integer
+fread1 <- function(filehandle) {
+    return(readBin(filehandle, integer(), n = 1, endian = "big"));
+}
 
+# read a short
+freadshort <- function(filehandle) {
+    return(readBin(filehandle, numeric(), n = 1, endian = "big"));
+}
 
+# read a 3 byte integer
 fread3 <- function(filehandle) {
     b1 = read_byte(filehandle);
     b2 = read_byte(filehandle);
@@ -37,6 +72,5 @@ read_byte <- function(filehandle) {
     return(b);
 }
 
-test_file = file.path("/home", "spirit", "data", "tim_only", "tim", "surf", "lh.area.fwhm0.fsaverage.mgh")
-#test_file = file.path("/Users", "timschaefer", "data", "tim_only", "tim", "surf", "lh.area.fwhm0.fsaverage.mgh")
+test_file = file.path(path.expand("~"), "data", "tim_only", "tim", "surf", "lh.area.fwhm0.fsaverage.mgh")
 read_fs_mgh_file(test_file)
