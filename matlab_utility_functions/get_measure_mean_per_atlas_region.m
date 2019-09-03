fc661f8f860e89eede1f2593f61a247004685707
Tim Schaefer <ts+code@rcmd.org>
2019-09-03 09:25:03 +0200

    fix critical indexing bug, wrong data was used

--- matlab_utility_functions/get_measure_mean_per_atlas_region.m
+++ matlab_utility_functions/get_measure_mean_per_atlas_region.m
@@ -11,15 +11,15 @@ function [atlas_regions, mean_per_region] = get_measure_mean_per_atlas_region(su
     [vertices, vertex_labels, colortable] = read_annotation(atlas_file_this_hemi);
     %atlas_regions = string(matlab.lang.makeValidName(colortable.struct_names));
     atlas_regions = string(colortable.struct_names);
-    
-    
+
+
     mean_per_region = zeros(length(atlas_regions), 1);
-    
+
     for region_idx = 1:length(atlas_regions)
         color_code_this_region = colortable.table(region_idx, 5);
-        vert_indices = find(vertex_labels(vertex_labels == color_code_this_region));
+        vert_indices = vertex_labels == color_code_this_region;
         mean_in_region = mean(measure_data(vert_indices));
         mean_per_region(region_idx, 1) = mean_in_region;
     end
-    
+
 end
